7eebdfccdf6903721508bfc019cd8305b210d370
vinaykannambadi <vinaykannambadi@gmail.com>
2020-05-07 19:31:07 +0530

    1 file modified

--- index.ts
+++ index.ts
@@ -1,81 +1,246 @@
-import { from } from 'rxjs'; 
+import { of, from } from 'rxjs'; 
 import { map, every, flatMap } from 'rxjs/operators';
 
 
-const myData = [
-	{
-		"id": "0001",
-		"type": "donut",
-		"name": "Cake",
-		"ppu": 0.55,
-		"batters":
-			{
-				"batter":
-					[
-						{ "id": "1001", "type": "Regular" },
-						{ "id": "1002", "type": "Chocolate" },
-						{ "id": "1003", "type": "Blueberry" },
-						{ "id": "1004", "type": "Devil's Food" }
-					]
-			},
-		"topping":
-			[
-				{ "id": "5001", "type": "None" },
-				{ "id": "5002", "type": "Glazed" },
-				{ "id": "5005", "type": "Sugar" },
-				{ "id": "5007", "type": "Powdered Sugar" },
-				{ "id": "5006", "type": "Chocolate with Sprinkles" },
-				{ "id": "5003", "type": "Chocolate" },
-				{ "id": "5004", "type": "Maple" }
-			]
-	},
-	{
-		"id": "0002",
-		"type": "donut",
-		"name": "Raised",
-		"ppu": 0.55,
-		"batters":
-			{
-				"batter":
-					[
-						{ "id": "1001", "type": "Regular" }
-					]
-			},
-		"topping":
-			[
-				{ "id": "5001", "type": "None" },
-				{ "id": "5002", "type": "Glazed" },
-				{ "id": "5005", "type": "Sugar" },
-				{ "id": "5003", "type": "Chocolate" },
-				{ "id": "5004", "type": "Maple" }
-			]
-	},
-	{
-		"id": "0003",
-		"type": "donut",
-		"name": "Old Fashioned",
-		"ppu": 0.55,
-		"batters":
-			{
-				"batter":
-					[
-						{ "id": "1001", "type": "Regular" },
-						{ "id": "1002", "type": "Chocolate" }
-					]
-			},
-		"topping":
-			[
-				{ "id": "5001", "type": "None" },
-				{ "id": "5002", "type": "Glazed" },
-				{ "id": "5003", "type": "Chocolate" },
-				{ "id": "5004", "type": "Maple" }
-			]
-	}
-];
-
-const source = from(myData).pipe(
-  map(x => x.batters.batter)).pipe(flatMap(val => val));
+const myData = {
+  "PolicyData": [
+    {
+      "id": "1",
+      "Vehicle": [
+        {
+          "ownership": "Owned",
+          "year": "2017",
+          "bodystyle": "Sedan 4 Doors",
+          "primarlyused": "Commute",
+          "model": "ROMEO 4C",
+          "vin": "KNMAT2MV0JP621059",
+          "id": "1",
+          "make": "ALFA",
+          "miles": "10000"
+        }
+      ],
+      "Driver": [
+        {
+          "firstname": "John M",
+          "address": "45 HANOI STRRET TULSA, OK 74102-2201",
+          "education": "",
+          "gender": "Female",
+          "dob": "09-18-1989",
+          "drivertype": "Licensed",
+          "id": "1",
+          "empstatus": "",
+          "age": "30",
+          "lastname": "",
+          "contactno": "",
+          "status": "Married"
+        },
+        {
+          "firstname": "O’BRIEN S",
+          "address": "45 HANOI STRRET TULSA, OK 74102-2201",
+          "education": "",
+          "gender": "Male",
+          "dob": "05-02-1982",
+          "drivertype": "Extended",
+          "id": "2",
+          "empstatus": "",
+          "age": "37",
+          "lastname": "",
+          "contactno": "",
+          "status": "Married"
+        },
+        {
+          "firstname": "COY S",
+          "address": "45 HANOI STRRET TULSA, OK 74102-2201",
+          "education": "",
+          "gender": "Male",
+          "dob": "09-07-2002",
+          "drivertype": "Unlicensed",
+          "id": "3",
+          "empstatus": "",
+          "age": "17",
+          "lastname": "",
+          "contactno": "",
+          "status": ""
+        }
+      ],
+      "Coverage": [
+        {
+          "bodyEachAccident": "$50,000",
+          "PDEachAccident": "$25,000",
+          "collision": "$1,000",
+          "UnInsuredBIEachPerson": "$25,000",
+          "bodyEachPerson": "$25,000",
+          "MPEachPerson": "$1,000",
+          "comprehensive": "$1,000",
+          "UnInsuredBIEachAccident": "$50,000"
+        }
+      ],
+      "Premium": [
+        {
+          "Car": "ALFA ROMEO 4B",
+          "currentpremium": "$540",
+          "name": "MIRINDA JONES",
+          "proposedpremium": "$10",
+          "switchsavings": "$20",
+          "savingpercentage": "10%"
+        }
+      ]
+    },
+    {
+      "id": "2",
+      "Vehicle": [
+        {
+          "ownership": "Owned",
+          "year": "2017",
+          "bodystyle": "Sedan 4 Doors",
+          "primarlyused": "Commute",
+          "model": "ROMEO 4C",
+          "vin": "KNMAT2MV0JP621057",
+          "id": "1",
+          "make": "ALFA",
+          "miles": "10000"
+        }
+      ],
+      "Driver": [
+        {
+          "firstname": "MIRINDA JONES",
+          "address": "45 HANOI STRRET TULSA, OK 74102-2201",
+          "education": "",
+          "gender": "Female",
+          "dob": "09-18-1989",
+          "drivertype": "Licensed",
+          "id": "1",
+          "empstatus": "",
+          "age": "30",
+          "lastname": "",
+          "contactno": "",
+          "status": "Married"
+        },
+        {
+          "firstname": "O’BRIEN S",
+          "address": "45 HANOI STRRET TULSA, OK 74102-2201",
+          "education": "",
+          "gender": "Male",
+          "dob": "05-02-1982",
+          "drivertype": "Extended",
+          "id": "2",
+          "empstatus": "",
+          "age": "37",
+          "lastname": "",
+          "contactno": "",
+          "status": "Married"
+        },
+        {
+          "firstname": "COY S",
+          "address": "45 HANOI STRRET TULSA, OK 74102-2201",
+          "education": "",
+          "gender": "Male",
+          "dob": "09-07-2002",
+          "drivertype": "Unlicensed",
+          "id": "3",
+          "empstatus": "",
+          "age": "17",
+          "lastname": "",
+          "contactno": "",
+          "status": ""
+        }
+      ],
+      "Coverage": [
+        {
+          "bodyEachAccident": "$50,000",
+          "PDEachAccident": "$25,000",
+          "collision": "$1,000",
+          "UnInsuredBIEachPerson": "$25,000",
+          "bodyEachPerson": "$25,000",
+          "MPEachPerson": "$1,000",
+          "comprehensive": "$1,000",
+          "UnInsuredBIEachAccident": "$50,000"
+        }
+      ],
+      "Premium": [
+        {
+          "Car": "ALFA ROMEO 4C",
+          "currentpremium": "$540",
+          "name": "John JONES",
+          "proposedpremium": "$30",
+          "switchsavings": "$40",
+          "savingpercentage": "30%"
+        }
+      ]
+    },
     
+{
+  "id": "3",
+  "Vehicle": [],
+  "Driver": [],
+  "Coverage": [],
+  "Premium": [
+    {
+      "Car": "ALFA ROMEO 4C",
+      "currentpremium": "$540",
+      "name": "Sagar",
+      "proposedpremium": "$5000",
+      "switchsavings": "$10000",
+      "savingpercentage": "16%"
+    }
+  ]
+    },
+    
+{
+  "id": "4",
+  "Vehicle": [],
+  "Driver": [],
+  "Coverage": [],
+  "Premium": [
+    {
+      "Car": "ALFA ROMEO 4C",
+      "currentpremium": "$540",
+      "name": "Chandra",
+      "proposedpremium": "$70",
+      "switchsavings": "$80",
+      "savingpercentage": "8%"
+    }
+  ]
+    },
+    
+{
+  "id": "5",
+  "Vehicle": [],
+  "Driver": [],
+  "Coverage": [],
+  "Premium": [
+    {
+      "Car": "ALFA ROMEO 4C",
+      "currentpremium": "$540",
+      "name": "Mirle",
+      "proposedpremium": "$50",
+      "switchsavings": "$60",
+      "savingpercentage": "60%"
+    }
+  ]
+    },
+    
+{
+  "id": "6",
+  "Vehicle": [],
+  "Driver": [],
+  "Coverage": [],
+  "Premium": [
+    {
+      "Car": "ALFA ROMEO 4C",
+      "currentpremium": "$540",
+      "name": "Vikram",
+      "proposedpremium": "$450",
+      "switchsavings": "$90",
+      "savingpercentage": "16%"
+    }
+  ]
+    }
+  ]
+}
 
 
-source.subscribe(val => console.log(val));
+
+const source = of(myData).pipe(
+  map(x => x.PolicyData.map(y =>y.Premium.map(z => z))));
+source.subscribe(val => val);
